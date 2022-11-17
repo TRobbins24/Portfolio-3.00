6b86af4de634fd2e07580e4201c9e026221a6f37
TylerR17 <84456198+TRobbins24@users.noreply.github.com>
2022-11-17 15:35:56 +0000

    Added secret

---
+++ code.js
@@ -0,0 +1,32 @@
+var pattern = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a', 'Enter'];
+var current = 0;
+
+var keyHandler = function(event) {
+
+  // If the key isn't in the pattern, or isn't the current key in the pattern, reset
+  if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
+    current = 0;
+    return;
+  }
+
+  // Update how much of the pattern is complete
+  current++;
+
+  // If complete, alert and reset
+  if (pattern.length === current) {
+    current = 0;
+
+
+    window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+
+
+
+
+
+
+  }
+
+};
+
+// Listen for keydown events
+document.addEventListener('keydown', keyHandler, false);
